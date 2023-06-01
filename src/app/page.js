0f538789e71a3d7251d6c9951e493a1a15e67762
MarcosVFG <marcosmarkin55@gmail.com>
2023-06-01 14:29:56 -0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <Image
+        src="/dedo.png"
+        width={200}
+        height={200}
+        alt="QR CODE"
+        className={styles.imagem}
+      />
+        <h1>Com amor, Marcos</h1>
+        <p className={styles.paragrafo}>Eddy, essa é uma pequena demonstração do carinho que tenho por você
+          você é uma pessoa excepcional, espero compartilhar mais momentos com você! 
+        </p>
+      </div>
+    </main>
+  )
+}
